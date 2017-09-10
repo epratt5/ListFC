7ee5ce23902cfd57a039e9924f8583a7ac7b410b
epratt5 <31831585+epratt5@users.noreply.github.com>
2017-09-10 19:00:08 -0400

    Pratt repo changes
    Emilie Pratt entered her code for the repo changes training.

--- ListFC.py
+++ ListFC.py
@@ -1,21 +1,21 @@
-# Author:  
-# Date:
-# Purpose:  
-
+# Author: Emilie Pratt
+# Date: 10 Sept 2017
+# Purpose: GitHub Training
 
 # Import ArcPy
 import arcpy
- 
 
 # Set the workspace
- 
+arcpy.env.workspace = "C:/Users/erbre/Desktop/Programming 606/Data/Assignment 2/Newark.gdb"
+path = arcpy.env.workspace
 
 # List all of the feature classes in the Newark geodatabase 
-
+newarkGDB = arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for fc in newarkGDB:
+    print fc
 
 # Print "Complete"
-
+print "Complete"
 
